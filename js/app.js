1e151f3fab6905fa673bb22e47ee82b3101a4a69
Andrés Osante <aosantea@ucenfotec.ac.cr>
2020-06-10 23:01:11 -0600

    project update and fixed parallax bug

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 const scrollTop = document.querySelector('.scroll-top-btn');
-const options = {};
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
+scrollTop.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
+const parallaxBg = document.querySelector('.parallax-bg');
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
     if (entry.isIntersecting && entry.target.id === 'form') {
       entry.target.classList.toggle('triggered');
     }
@@ -9,31 +15,31 @@ const observer = new IntersectionObserver(entries => {
       scrollTop.classList.add('hide');
       window.addEventListener('scroll', parallax, {
         capture: false,
-        passive: true
+        passive: true,
       });
     } else {
       if (entry.target.id === 'header') {
         scrollTop.classList.remove('hide');
         window.removeEventListener('scroll', parallax, {
           capture: false,
-          passive: true
+          passive: true,
         });
       }
     }
   });
-}, options);
+}, {});
 
 const contactForm = document.querySelector('.animated-form');
 const sectionHero = document.querySelector('.parallax-bg');
 const footer = document.querySelector('#footer');
 const sections = [contactForm, sectionHero, footer];
-sections.forEach(section => observer.observe(section));
+sections.forEach((section) => observer.observe(section));
 
 // TODO: REPLACE ALL SCROLL EVENT LISTENERS WITH INTERSECTION OBSERVER
 
 //remove :before content from skill sections in larger screens---------
 function removeBeforeContent() {
-  document.querySelectorAll('.skill-section').forEach(e => {
+  document.querySelectorAll('.skill-section').forEach((e) => {
     window.innerWidth > 980
       ? e.classList.remove('before-border')
       : e.classList.add('before-border');
@@ -42,7 +48,7 @@ function removeBeforeContent() {
 removeBeforeContent(), window.addEventListener('resize', removeBeforeContent);
 
 // Form validation
-document.querySelector('form').addEventListener('input', e => {
+document.querySelector('form').addEventListener('input', (e) => {
   console.log(e.target), e.target.classList.remove('error');
 });
 
@@ -54,25 +60,24 @@ function validateForm(e) {
       ? swal({
           title: 'Error',
           text: 'Please fill out all fields before submitting',
-          icon: 'error'
+          icon: 'error',
         })
       : (o = !0),
     o
   );
 }
-const validateInputs = e => {
+const validateInputs = (e) => {
   let o = !1;
   return (
-    e.forEach(e => {
+    e.forEach((e) => {
       '' == e.value && (e.classList.add('error'), (o = !0));
     }),
     o
   );
 };
 
-const parallax = _ => {
-  const e = document.querySelector('.parallax-bg');
+const parallax = () => {
   let o = window.scrollY;
-  e.style.backgroundPosition = 'center ' + 0.35 * o + 'px';
+  parallaxBg.style.backgroundPosition = 'center ' + 0.35 * o + 'px';
 };
 // add this event listener only if hero section is intersecting, remove if not.
